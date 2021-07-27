652544e438fffb9c7004f902bf8357381923d5e9
Fabian Reinders <fabian@fabiancdng.com>
2021-07-27 11:45:28 +0200

    ✨ Front-End: Implement Shortlink Creation
    Implement shortlink creation on the React front end. This feature
    utilizes the /api/shortlink/create API endpoint.

--- web/src/components/CreateShortlink.js
+++ web/src/components/CreateShortlink.js
@@ -1,8 +1,31 @@
-import { FiLink } from "react-icons/fi"
+import { Button, HStack, Input, InputGroup, InputLeftElement, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text, useClipboard, useDisclosure } from "@chakra-ui/react"
+import { useState } from "react"
+import { FiCheck, FiLink } from "react-icons/fi"
 import QuickAction from "./QuickAction"
-
 const CreateShortlink = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [shortlink, setShortlink] = useState('')
+    const { hasCopied, onCopy } = useClipboard(shortlink)
+
+    const createShortlink = async (link) => {
+        var short = await fetch('/api/shortlink/create', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({link: link})
+        })
+
+        short = await short.json()
+        short = window.location.href + short.short
+        setShortlink(short)
+
+        onOpen()
+    }
+
     return (
+    <>
         <QuickAction
             title="Quickly create a shortlink"
             subtitle={<b>Paste. Click the button. Done.</b>}
@@ -10,7 +33,30 @@ const CreateShortlink = () => {
             color="green"
             placeholder="Paste your long link here"
             buttonLabel="Shorten"
+            onOpen={onOpen}
+            handlerFunction={createShortlink}
         />
+
+        <Modal size="4xl" onClose={onClose} isOpen={isOpen}>
+                <ModalOverlay />
+                <ModalContent pb={5}>
+                    <ModalHeader><HStack color="green.300"><FiCheck size={28} /> <Text>Shortlink created!</Text></HStack></ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <InputGroup>
+                            <InputLeftElement
+                                pointerEvents="none"
+                                children={<FiLink color="gray.300" />}
+                            />
+                            <Input isReadOnly={true} type="text" value={shortlink} />
+                            <Button onClick={onCopy} ml={2}>
+                                {hasCopied ? "Copied" : "Copy"}
+                            </Button>
+                        </InputGroup>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+    </>
     )
 }
 
--- web/src/components/QuickAction.js
+++ web/src/components/QuickAction.js
@@ -1,13 +1,16 @@
 import { Box, Button, Heading, HStack, Input, Text } from "@chakra-ui/react"
+import { useState } from "react"
 
-const QuickAction = ({title, subtitle, icon, color, placeholder, buttonLabel}) => {
+const QuickAction = ({ title, subtitle, icon, color, placeholder, buttonLabel, handlerFunction }) => {
+    const [link, setLink] = useState('')
+
     return (
         <Box mt={10} width="80%" height="min" p={8} borderWidth={1} borderRadius={8} boxShadow="lg">
             <Heading size="lg">{ title }</Heading>
             <Text mt={0.5} pl={0.5} size="xs">{ subtitle }</Text>
             <HStack mt={3}>
-                <Input placeholder={ placeholder } size="md" />
-                <Button colorScheme={color} variant="outline" leftIcon={icon}>{ buttonLabel }</Button>
+                <Input placeholder={ placeholder } size="md" value={link} onChange={e => setLink(e.target.value)} />
+                <Button colorScheme={color} variant="outline" leftIcon={icon} onClick={e => {handlerFunction(link)}}>{ buttonLabel }</Button>
             </HStack>
         </Box>
     )
